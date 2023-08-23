420cbcde2f542f73a188f598185a7c822b78d8c2
OPJMendez <o.p.logisticojjmendez@gmail.com>
2023-08-23 09:04:59 -0500

    siu

--- widgets/lib/main.dart
+++ widgets/lib/main.dart
@@ -12,29 +12,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: Widgets_Simple(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+// ignore: camel_case_types
+class Widgets_Simple extends StatelessWidget {
+  const Widgets_Simple({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        // ListView(
+            //   scrollDirection: Axis.horizontal,
+            //   children: const <Widget>[],
+            // ),
         margin: const EdgeInsets.only(top: 400, left: 100),
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(0.0),
         child: Row(
           children: [
             Column(
               children: [
-                const Icon(Icons.ac_unit_sharp),
+                const Icon(Icons.ac_unit_sharp, size: 30, color: Color.fromARGB(255, 173, 140, 140)),
                 Container(
                   width: 70,
                   height: 50,
                   child: const Center(
-                    child: Text('Copo'),
+                    child: Text('Copo', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(width: 10),
+            Column(
+            
+              children: [
+                const Icon(Icons.shopping_bag, size: 30, color: Color.fromARGB(255, 173, 173, 172),),
+                Container(
+                  width: 90,
+                  height: 50,
+                  child: const Center(
+                    child: Text('Compras', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
                   ),
                 ),
               ],
@@ -42,26 +61,12 @@ class LoginPage extends StatelessWidget {
             const SizedBox(width: 10),
             Column(
               children: [
-                const Icon(Icons.shopping_bag),
+                const Icon(Icons.access_time, size: 30, color: Color.fromARGB(255, 92, 63, 63)),
                 Container(
                   width: 70,
                   height: 50,
                   child: const Center(
-                    child: Text('Comprar'),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 10),
-            Column(
-              children: [
-                const Icon(Icons.access_time),
-                Container(
-                  width: 70,
-                  height: 50,
-                  // color: Colors.blue,
-                  child: const Center(
-                    child: Text('Reloj'),
+                    child: Text('Reloj', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
                   ),
                 ),
               ],
